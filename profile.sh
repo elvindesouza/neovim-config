2af1f8f53d62ef8af58dae02bf623e988a2fb383
elvindesouza <elvindesouza@gmail.com>
2023-11-05 02:44:01 +0530

    november new appimage

--- profile.sh
+++ profile.sh
@@ -4,8 +4,4 @@ rm -f nvim.appimage
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod +x nvim.appimage
 
-nvim --startuptime vim.log -c 'q'
-batcat vim.log
-rm vim.log
-
 time zsh -i -c exit
